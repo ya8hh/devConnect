a2923bc2aef17666142674c44e59e5c353901fb8
Yash Pal <pyash3137@gmail.com>
2025-04-20 13:41:39 +0530

    fixed the connection flow

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express =require('express');
-require('./config/database')
+const connectDb = require('./config/database')
 const app = express();
-
-
-
-app.listen(3000,(req,res)=>{
-    console.log(`server listening at${3000}`);
+connectDb().then(()=>{console.log("db connected")
+    app.listen(3000,(req,res)=>{
+        console.log(`server listening at${3000}`);
+    })    
 })
+.catch((err)=>{console.log("db not connected",err.message)})
+
--- src/config/database.js
+++ src/config/database.js
@@ -3,5 +3,4 @@ require('dotenv').config();
 const connectDb =async()=>{
     await mongoose.connect(process.env.MONGO)
 }
-connectDb().then(()=>{console.log("db connected")})
-.catch((err)=>{console.log("db not connected",err.message)})
+module.exports = connectDb;
