b195b012c50a2aa8a92f8e352d5038182cec1854
Yash Pal <pyash3137@gmail.com>
2025-04-24 18:47:15 +0530

    added edit password api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const profileRouter =express.Router();
 const { userAuth } = require('../middlewares/auth');
-const {validateEditFields} =require("../utils/validation")
+const {validateEditFields} =require("../utils/validation");
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 //profile
 profileRouter.get("/profile/view",userAuth,async(req,res)=>{
@@ -31,6 +33,22 @@ profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
         res.send("try again, "+error.message);
     }
 })
-
+//password edit
+profileRouter.patch("/profile/edit/password",userAuth,async (req,res)=>{
+   try {
+    const {oldPass,newPass}=req.body;
+    const user = await User.findById(req.user._id).select("+password");
+    const isTrue = await bcrypt.compare(oldPass,user.password);
+    if(!isTrue){
+        throw new Error("credentials not correct");
+    }
+    const passwordHash = await bcrypt.hash(newPass,10)
+    user.password =passwordHash;
+    await user.save();
+    res.status(200).send("Password Updated");
+   } catch (error) {
+        res.status(400).send("Cant update "+error.message);
+   }
+})
 
 module.exports =profileRouter;
