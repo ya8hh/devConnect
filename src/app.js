9d559740e4d806f9dec4c325321b96aadc59b40f
Yash Pal <pyash3137@gmail.com>
2025-05-10 15:35:21 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const app = express();
 const connectDb = require('./config/database')
 const User =require("./models/user.model")
 const cookieParser = require("cookie-parser");
+const cors = require('cors')
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
